c8c019e1530cd9073e42e3ca7bc37b7e9af18a5d
Rukaiya <rukaiya.20241784@iit.ac.lk>
2026-03-19 15:13:59 +0530

    Channeling routes and constants update

---
+++ frontend/app/routes/channelingConstants.ts
@@ -0,0 +1,33 @@
+export const SPECIALIZATIONS = [
+  "General Practitioner",
+  "Cardiologist",
+  "Dermatologist",
+  "Neurologist",
+  "Orthopedic Surgeon",
+  "Pediatrician",
+  "Psychiatrist",
+  "Gynecologist",
+  "Ophthalmologist",
+  "ENT Specialist",
+  "Diabetologist",
+  "Urologist",
+];
+
+export const HOSPITALS = [
+  "Colombo National Hospital",
+  "Lanka Hospital",
+  "Asiri Medical",
+  "Nawaloka Hospital",
+  "Durdans Hospital",
+  "Hemas Hospital",
+  "Central Hospital",
+  "Ninewells Hospital",
+];
+
+export const TIMES = [
+  "08:00 AM","08:30 AM","09:00 AM","09:30 AM","10:00 AM","10:30 AM",
+  "11:00 AM","11:30 AM","12:00 PM","12:30 PM",
+  "02:00 PM","02:30 PM","03:00 PM","03:30 PM",
+  "04:00 PM","04:30 PM","05:00 PM","05:30 PM","06:00 PM","06:30 PM",
+  "07:00 PM","07:30 PM","08:00 PM",
+];
